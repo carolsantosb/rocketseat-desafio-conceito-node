e51557239e771a79978c2217764fac6596107d03
Caroline Bezerra <caroline.santos1172@gmail.com>
2020-06-17 14:54:40 -0300

    atualizacao-rotas-desafio-nodejs

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  const repository = repositories.find((repsitory) => repository.id === id);
+  const repository = repositories.find((repository) => repository.id === id);
 
   if (!repository) {
     return response.status(400).json({error: 'Repository not found.'})
